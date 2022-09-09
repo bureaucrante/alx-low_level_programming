65bda7424579850f5ae29346c1b4d7130d1de6a1
bureaucrante <schwinston00@gmail.com>
2022-09-08 19:08:26 -0700

    art

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ *main - Entry point
  *
  *Description: 'printing simple text with printf'
- * 
- *Return: Always 0 (Success)      
+ *
+ *Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	
 	printf("with proper grammar, but the outcome is a piece of art,\n");
