f2f1447140fa94f857a07ac1dbcadfd84abed73e
bureaucrante <schwinston00@gmail.com>
2022-09-08 18:49:53 -0700

    puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
-/* to write a program that displays text using puts, not printf. I wonder why. */
-
 #include <stdio.h>
 
-int main()
+int main(void)
 {
 	/*string initialisation*/
 	char Mystr[] = "Programming is like building a multilingual puzzle.";
