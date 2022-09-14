eee339ddf089673b9ede05480cdb08e1c41bd26f
bureaucrante <schwinston00@gmail.com>
2022-09-14 12:56:05 -0700

    some bs

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,14 @@
 /**
  * main - Print _putchar followed by a new line
  *
- * Description: Prints putchar and a new line if you can believe it 
+ * Description: Prints putchar and a new line if you can believe it
  *
  * Return: 0 if successful
  */
 
 int main(void)
 {
-	printf ("_putchar\n");
+	printf("_putchar\n");
 
-	return(0);
+	return (0);
 }
