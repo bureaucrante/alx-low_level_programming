e5b9c1559a8512c5e053bd921eadc67935f3dfad
bureaucrante <schwinston00@gmail.com>
2022-09-14 22:19:52 -0700

    case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - Checks for lowercase characters
- * @c - is the character to be case-checked
+ * @c: is the character to be case-checked
  *
  * Return: 1 for lowercase characters, 0 for not lowercase characters.
  */
