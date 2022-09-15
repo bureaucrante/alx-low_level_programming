fabc930d9be4b008cadfc3b4f4d358b12232e5d3
bureaucrante <schwinston00@gmail.com>
2022-09-14 23:58:43 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,14 +3,15 @@
  * print_last_digit - prints the last digit of a number
  * @num: variable to be checked
  *
- * */
+ * Return: last digit of number
+ */
 
 int print_last_digit(int num)
 {
 	int lastDigit;
-	
+
 	lastDigit = num % 10;
-	
+
 	if (lastDigit < 0)
 	{
 		lastDigit = lastDigit * -1;
