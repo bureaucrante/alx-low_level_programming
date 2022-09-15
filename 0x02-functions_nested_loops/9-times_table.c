3805226133803909a003b4e7065d5349b981c01f
bureaucrante <schwinston00@gmail.com>
2022-09-15 00:44:28 -0700

    multi

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/*times_table*/
+
+void times_table(void)
+{
+	int n;
+
+	for  (n = 0; n <= 9; n++ )
+	{
+		int c;
+
+		for (c=0; c =< 9; c++)
+		{
+			int multi;
+
+			multi = (n * c);
+
+			_putchar(multi);
+		}
+	}
+	return (0);
+}
+
