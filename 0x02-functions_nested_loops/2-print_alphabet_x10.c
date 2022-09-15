efe70c6c9cffc33399c2fda7016803684ea714d2
bureaucrante <schwinston00@gmail.com>
2022-09-14 22:00:16 -0700

    10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ * print_alphabet - prints the alphabet
+ *
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int l;
+	for (l = 0; l <= 10; l++ )
+	{
+		char x = 'a';
+		while ( x <= 'z')
+		{
+			_putchar(x);
+			x++;
+		}
+	}
+	_putchar('\n');
+}
