e23f2227ece41ef6d4f27a6203bea44326906252
bureaucrante <schwinston00@gmail.com>
2022-09-14 22:46:15 -0700

    positive or negative

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign: prints the sign of the number
+ * @n: the value for the integer
+ *
+ * Return: 1 for positive; 0 for zero; -1 for negative.
+ */
+
+int print_sign(int n)
+{
+	int n;
+
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+
+	} else if (n = 0)
+	{
+		_putchar(48);
+		return (0);
+
+	} else if (n > 0)
+		_putchar(45);
+		return (-1);
+}
