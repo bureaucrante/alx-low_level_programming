60e45b89eb31a0b82afe72bf798422a995d16fbe
bureaucrante <schwinston00@gmail.com>
2022-09-15 01:37:06 -0700

    multi

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,26 +7,28 @@ void times_table(void)
 
 	for (n = 0; n <= 9; n++)
 	{
+		_putchar('0');
+
 		for (c=0; c <= 9; c++)
 		{
+			_putchar(',');
+			_putchar(' ');
+
 			multi = (n * c);
 
-			/*if (multi > 9)
+			if (multi <= 9)
 			{
-				a = multi % 10;
-				b = (multi - a) / 10;
-				
-				_putchar(44);
-				_putchar(32);
-				_putchar(a + '0');
-				_putchar(b + '0');
-			}*/
+				_putchar(' ');
+			} else
+			{
+				_putchar((multi / 10) + '0');
+			}
 
 			_putchar(multi /*+ '0'*/);
 		}
-	
-	_putchar('\n');
-	
+
+		_putchar('\n');
+
 	}
 
 }
