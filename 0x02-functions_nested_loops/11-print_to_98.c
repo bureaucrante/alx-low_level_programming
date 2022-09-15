ae87dfbe4b691ac572bde0059d384898da16ee65
bureaucrante <schwinston00@gmail.com>
2022-09-15 09:19:32 -0700

    now?

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,33 +12,19 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			_putchar(n);
-	
-			if (n != 98)
-			{
-				_putchar(',');
-			}
-			n++;
+			printf("%d, ", n++);
 		}
-	}
-
-	else if (n >= 98)
-	{
-		while (n >= 98)
-		{
-			_putchar(n);
-
-			if (n != 98)
-			{
-				_putchar(',');
-			}
-			n--;
-		}
+		printf("%d\n", n);
 	}
 
 	else
 	{
-		_putchar(n);
+		while (n >= 98)
+		{
+			printf("%d, ", n--);
+		}
+		printf("%d\n", n);
+
 	}
-	_putchar('\n');
+
 }
