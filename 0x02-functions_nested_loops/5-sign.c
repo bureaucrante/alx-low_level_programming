b5fa7f23489a0be9eb1b694744071f3f2555d7b3
bureaucrante <schwinston00@gmail.com>
2022-09-14 22:52:19 -0700

    positive or negative

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,15 +15,15 @@ int print_sign(int n)
 		return (1);
 	}
 	
-	if (n = 0)
-	{
-		_putchar(48);
-		return (0);
-	}
-	
-	if (n < 0)
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
+	}
+	
+	else
+	{
+		_putchar(48);
+		return (0);
 	}
 }
