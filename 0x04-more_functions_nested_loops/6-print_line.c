b7e261da71bb2ce60bc88b0cba18c07bebf857d0
bureaucrante <schwinston00@gmail.com>
2022-09-16 15:26:09 -0700

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints a series of underscores(_), in order to form a line.
+ * @n - the number of times the loop will run
+ * @i - the length of the lin 
+ *
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		while ((i > 0) && (i <= n))
+		{
+			_putchar(95);
+			i++;
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/linecheck.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int i;
+
+	i = 7;
+
+	for (n = 0; n < i; n++)
+	{
+		printf("_");
+	}
+
+	printf("\n");
+
+}
