7168edc308f41493deb6395f9bc1dabc02890c91
bureaucrante <schwinston00@gmail.com>
2022-09-16 18:35:59 -0700

    triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - i; j++)
+			{
+				_putchar(32);
+			}	
+
+			for (k = 1; k <= i; k++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
+
