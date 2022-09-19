5561f70af5ff00e8ee441e54ff0300e8fcd646d3
bureaucrante <schwinston00@gmail.com>
2022-09-19 14:38:23 -0700

    reset value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the value at the address of a pointer to 98
+ * @*n: the pointer to be altered.
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+Pointers, arrays and strings
+
+Learning the essense and execution of pointers, arrays and strings. 
