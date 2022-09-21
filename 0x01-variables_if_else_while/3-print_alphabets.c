128bb7bb7e7ccfd6efd3c05cba453bda0802b491
bureaucrante <schwinston00@gmail.com>
2022-09-21 13:21:09 -0700

    alphabets encore

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ */
+
+int main (void)
+{
+	int i, x;
+
+	i = 97;
+	x = 65;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	while (x < 90)
+	{
+		putchar(x);
+		x++;
+	
+	}
+	putchar('\n');
+}
