98e7375b223138f4f7d2f2acc5a962b2f8577eef
bureaucrante <schwinston00@gmail.com>
2022-09-21 13:59:04 -0700

    print numbers sans char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - main
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
