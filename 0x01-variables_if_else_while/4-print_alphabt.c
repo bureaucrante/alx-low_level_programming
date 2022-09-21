dcbcda40b8b3a789ba46f8e0d633e993018b92fa
bureaucrante <schwinston00@gmail.com>
2022-09-21 13:34:14 -0700

    some alphabets

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,13 +5,20 @@
  * Return: Always 0
  */
 
-int main (void)
+int main(void)
 {
 	int i;
 	i = 97;
 
 	while (i <= 122)
 	{
-		if (i == 103 && i == )
+		if (i != 113 && i != 101)
+		{
+			putchar(i);
+		}
+		i++;
 	}
+	putchar('\n');
+
+	return (0);
 }
