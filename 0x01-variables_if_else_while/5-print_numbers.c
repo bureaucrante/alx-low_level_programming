b4e608c53fe2fcdb297c76dd1ba65f6fa7e0efc9
bureaucrante <schwinston00@gmail.com>
2022-09-21 13:53:30 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
