2d3df409d53ef724fcb43ccd21223a3d6a79f2ed
bureaucrante <schwinston00@gmail.com>
2022-09-20 17:45:11 -0700

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string.
+ * @src: source string.
+ * @dest: destination string.
+ *
+ * Return: destination string.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int n;
+
+	n = 0;
+
+	while (src[n] != '\0')
+	{
+		dest[n] = src[n];
+		n++;
+	}
+
+	return (dest);
+}
