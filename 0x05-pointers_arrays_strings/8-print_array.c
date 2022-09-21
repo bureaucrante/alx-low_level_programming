59ab953c13105c44c96b923f6c04e83772bf2ffa
bureaucrante <schwinston00@gmail.com>
2022-09-20 17:13:24 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints the elements within an array
+ * @a: the array to evaluated
+ * @n: the total number of elements within the array (the limit)
+ */
+
+void print_array(int *a, int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		if (a[j] != '\0')
+		{
+			printf("%d, ", a[j]);
+		}
+	}
+	printf("\n");
+}
