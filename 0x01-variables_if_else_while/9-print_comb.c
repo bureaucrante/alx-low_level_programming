e4c0d30f096a085dafe27ce6a4563d566afe6667
bureaucrante <schwinston00@gmail.com>
2022-09-21 14:34:24 -0700

    spaces,

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - programme to print numbers with spaces and commas
+ * Return: Always 0;
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+
+		if (i < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
