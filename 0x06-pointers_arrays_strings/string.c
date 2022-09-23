649db12b27695239c6a62f4fedd5e9540585de76
bureaucrante <schwinston00@gmail.com>
2022-09-23 14:40:11 -0700

    to upper

--- 0x06-pointers_arrays_strings/string.c
+++ 0x06-pointers_arrays_strings/string.c
@@ -5,28 +5,39 @@ int main()
 {
 	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 	
-	int i, j, k, n;
+	int i, j, k;
 
 	int temp;
 	
-	n = 13;
-	/*while (a[n] != '\0')
+	int *n = a;
+	
+	int l = 13;
+
+	for (i = 1; i < l; i++)
 	{
 		n++;
-	}*/
-
-	for (j = 0; j <= (n/2); j++)
-	{
-		for (i = 0; i <= (n/2); i++)
-		{
-			temp = a[i];
-			a[i] = a[n - 1 - i];
-			a[n - 1 - i] = temp;
-		}
-		printf("%d\n", a[i]);
 	}
 
-	putchar('\n');
+	for (j = 0; j < i/2; i++)
+	{
+		temp = a[j];
+		a[j] = *n;
+		*n = temp;
+		n--;
+	}
+
+	for (k = 0; k < l; k++)
+	{
+//		if (count != 0)
+//		{
+//			printf(", ");
+//		}
+		printf("%d", a[k]);
+	}
+//	printf("\n");
+
+
+//	printf("%d, %d\n", *n, a[12]);
 
 	return (0);
 }
