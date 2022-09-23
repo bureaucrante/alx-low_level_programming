35b59883ea4a14456f1aac22fa0638aafba0516d
bureaucrante <schwinston00@gmail.com>
2022-09-22 21:11:09 -0700

    core dumped??

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, y = 0, z = 0;
+
+	int len1 = 0;
+
+	int len2 = 0;
+
+	while (dest[y] != '\0')
+	{
+		len1++;
+		y++;
+	}
+
+	while (src[z] != '\0')
+	{
+		len2++;
+		z++;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		dest[len1 + i] = src[i];
+
+		if (src[i] == len2)
+		{
+			break;
+		}
+	}
+	return (dest);
+}
