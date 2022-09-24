f7d3358fd25d1ac6709af8ceb94144edc9db25c9
bureaucrante <schwinston00@gmail.com>
2022-09-24 10:14:35 -0700

    now????

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,20 +9,22 @@
 
 char *leet(char *str)
 {
-	int i, j = 0;
+	int i = 0, j = 0;
 
 	char big[] = {'A', 'a', 'E', 'e', 'O', 'o', 'T', 't', 'L', 'l'};
 	char num[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
 		for (j = 0; j < 10; j++)
 		{
 			if (str[i] == big[j])
 			{
 				str[i] = num[j];
+				break;
 			}
 		}
+		i++;
 	}
 	return (str);
 }
