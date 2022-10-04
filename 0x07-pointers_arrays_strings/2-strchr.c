249fae059a95c4a841f6f17d9fe4b23291487e4a
bureaucrante <schwinston00@gmail.com>
2022-10-04 13:19:07 -0700

    function to find a characer in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,23 @@
 #include "main.h"
 
 /**
+ * _strchr - finds a specific character in a string,
+ * @s: the string to be evaluated.
+ * @c: the character to be found.
+ * Return: string or null.
  */
 
 char *_strchr(char *s, char c)
 {
-	/* code block */
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+
+	return ('\0');
 }
