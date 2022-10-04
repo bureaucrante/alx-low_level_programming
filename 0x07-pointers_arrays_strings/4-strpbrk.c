38e33ce9f355feda44a9880c4c6cdc9a9f668c30
bureaucrante <schwinston00@gmail.com>
2022-10-04 14:07:26 -0700

    function to search a string for any character in a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,9 +1,31 @@
 #include "main.h"
 
 /**
+ * _strpbrk - finds the first instance of a character/byte from a set.
+ * @s: the string to be searched.
+ * @accept: pointer to array containing set of bytes/characters.
+ *
+ * Return: String at first instance or NULL.
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-	/* code block */
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (s[i] == accept[n])
+			{
+				return (s + i);
+			}
+		}
+	}
+	if (s[i] == accept[n])
+	{
+		return (s + i);
+	}
+
+	return ('\0');
 }
