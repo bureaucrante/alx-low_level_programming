0c4827f6827b9d6e24897ed40b5a4fcb72b8cdfd
bureaucrante <schwinston00@gmail.com>
2022-10-04 13:56:36 -0700

    function to print the length of a substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,36 @@
 #include "main.h"
 
 /**
+ * _strspn - gets the length of a prefix substring,
+ * @s: the source string,
+ * @accept: the array of of bytes in the prefix sub.
+ * Return: length of string.
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	/* code block */
+	unsigned int i, j, lenx = 0;
+
+	int flag;
+
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		flag = 1;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				flag = 0;
+			}
+		}
+
+		if (flag == 1)
+		{
+			break;
+		}
+		lenx++;
+	}
+	return (lenx);
 }
