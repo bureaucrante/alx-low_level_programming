88523786668ba8fdb6a303b36d7dca7016a83997
bureaucrante <schwinston00@gmail.com>
2022-10-05 17:27:19 -0700

    freed the greed

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees allocated memory.
+ * @grid: pointer to the location of allocated memory.
+ * @height: number of rows.
+ */
+
+void free_grid(int **grid, int height);
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+}
