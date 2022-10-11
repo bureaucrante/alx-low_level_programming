095a1ecf5135a38d55eca0a17eae1f9f31139006
bureaucrante <schwinston00@gmail.com>
2022-10-10 17:31:53 -0700

    dog info

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - datatype for dog information.
+ * @name: name of dog,
+ * @age: age of dog,
+ * @owner: owner of dog.
+ */
+
 struct dog
 {
 	char *name;
