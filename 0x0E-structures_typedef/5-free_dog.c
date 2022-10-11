0e14dc1beef971429183e4c4c297d3dbb20f172b
bureaucrante <schwinston00@gmail.com>
2022-10-10 20:01:21 -0700

    the dogs are free

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * free_dog - frees all memory allocated to dog_t objects
+ * @d: the dog to be set free.
+ */
+
 void free_dog(dog_t *d)
 {
 	if (d == 0)
