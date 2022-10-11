bd9ad0bf8e892ab4825560bbfa889c28d5f57b86
bureaucrante <schwinston00@gmail.com>
2022-10-10 18:21:20 -0700

    the dog should be printed

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,26 +4,26 @@
 /**
  * print_dog - function that prints the members of a struct variable
  * @d: pointer to the location of variable elements.
+ *
+ * Return: void
  */
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
+	if (d)
+	{
 
-	if (d->name == NULL)
-		printf("Name: nil\n");
-	else
-		printf("Name: %s\n", (*d).name);
+		if (d->name == NULL)
+			printf("Name: nil\n");
+		else
+			printf("Name: %s\n", (*d).name);
 
-	if (d->age == 0)
-		printf("Age: 0\n");
-	else
 		printf("Age: %f\n", (*d).age);
 
-	if (d->owner == NULL)
-		printf("Owner: nil\n");
-	else
-		printf("Owner: %s\n", (*d).owner);
+		if (d->owner == NULL)
+			printf("Owner: nil\n");
+		else
+			printf("Owner: %s\n", (*d).owner);
+	}
 }
 
