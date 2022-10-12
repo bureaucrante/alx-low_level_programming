7af63656025d73c53da17538208307c4d781370e
bureaucrante <schwinston00@gmail.com>
2022-10-12 12:28:24 -0700

    the executor

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "function_pointers.h"
 
 /**
@@ -5,7 +6,6 @@
  * @array: the array.
  * @size: size of the array.
  * @action: pointer to intended function.
- * @int: ...
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
