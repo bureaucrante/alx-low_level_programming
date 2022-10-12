415bcc1731c7631d0276db8fb129efd950220c60
bureaucrante <schwinston00@gmail.com>
2022-10-12 12:23:57 -0700

    the executor

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,13 +6,14 @@
  * @array: the array.
  * @size: size of the array.
  * @action: pointer to intended function.
+ * @int: ...
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array == 0 && action == 0)
+	if (array == 0 || action == 0)
 		return;
 
 	for (i = 0; i < size; i++)
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,5 +2,7 @@
 #define _FUNCTION_POINTERS_H_
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int _putchar(char c);
 
 #endif
