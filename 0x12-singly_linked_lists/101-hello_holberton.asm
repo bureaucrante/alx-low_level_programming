f55854afa9c9ab4dffa89e1eedaf849c3260ad84
bureaucrante <schwinston00@gmail.com>
2022-10-21 10:48:32 -0700

    Hello, assembly world

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,16 @@
+section .text
+
 	global main
 	extern printf
 
-section .text
-
 main:
-	mov rdi, msg 
-	mov rax, 0
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
 	call printf
-	mov rax, 0
+	mov eax, 0
 	ret
 
 section .data
-	msg db "Hello, World", 10
+	msg db "Hello, World", 0
+	fmt db "%s", 10, 0
