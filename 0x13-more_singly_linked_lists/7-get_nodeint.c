966adbf8ef5c1271877049db4b0333a2d04bed90
bureaucrante <schwinston00@gmail.com>
2022-10-25 10:55:44 -0700

    the node at index will be printed

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -17,7 +17,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (i = 0; i < index; i++)
 	{
 		ptr = ptr->next;
-		if (ptr->next == NULL)
+		if (ptr == NULL)
 			return (NULL);
 	}
 
