7695737642a946d473218f61add4a0f2837f1714
bureaucrante <schwinston00@gmail.com>
2022-10-25 10:51:04 -0700

    the node at index will be printed

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - accesses a list node at the given index.
+ * @head: pointer to the beginning of the list.
+ *
+ * Return: node at location.
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *ptr;
+	unsigned int i;
+
+	ptr = head;
+	if (head == NULL)
+		return (NULL);
+
+	for (i = 0; i < index; i++)
+	{
+		ptr = ptr->next;
+		if (ptr->next == NULL)
+			return (NULL);
+	}
+
+	return (ptr);
+}
