32a02d864c842514445841a089855bac4c62a3a9
bureaucrante <schwinston00@gmail.com>
2022-11-11 02:13:29 -0800

    the file will be created

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,10 +11,10 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, l;
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (!filename)
 		return (-1);
 
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
